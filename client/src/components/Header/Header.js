d5369bac361f1263b96af768f278df2d2a226390
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-03 16:08:11 +0300

    working on header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Header(){
     return(
-        <h1>Header page</h1>
+        <header>
+            <section className="logo">
+                <h1>Avtoborsa</h1>
+            </section>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Log in</a></li>
+                <li><a href="/">Register</a></li>
+            </ul>
+        </header>
     )
 }
 
