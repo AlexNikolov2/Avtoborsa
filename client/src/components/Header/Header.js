120bf82688208d8e9a3cda6c33fc4ce97180641f
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-05 00:51:57 +0300

    styling the header, i want it nice

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,9 +8,15 @@ function Header(){
                 <h1>Avtoborsa</h1>
             </section>
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/">Log in</a></li>
-                <li><a href="/">Register</a></li>
+                <li><a href="/"><i class="fa-solid fa-house"></i>Home</a></li>
+                <li><a href="/"><i class="fa-solid fa-right-to-bracket"></i>Login</a></li>
+                <li><a href="/"><i class="fa-solid fa-user-plus"></i>Sign up</a></li>
+                {//<li><a href="/"><i class="fa-solid fa-square-plus"></i>Create</a></li>
+}
+                {//<li><a href="/"><i class="fa-solid fa-address-card"></i>Profile</a></li>
+}
+                {//<li><a href="/"><i class="fa-solid fa-right-from-bracket"></i>Log out</a></li>
+}
             </ul>
         </header>
     )
