01031505dd64b9c50d7e8abd5bc7d9ca1ab76bf9
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-05 21:30:49 +0300

    yeah, working on footer, defining more accurate styling for the header section and not only

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer(){
+    return(
+        <footer>
+            <h1>This site is project for Softuni.</h1>
+        </footer>
+    )
+}
+
+export default Footer;
