18387d9f90386aabd3b11e4b247071e1ad0b56a6
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-07 02:09:43 +0300

    Landing page done

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -7,6 +7,14 @@ function LandingPage(){
         <section className="landing">
             <img src={landing} alt=""/>
             <h1 className="landing-up">Yet to find the car you want?</h1>
+            <h1 className="landing-center">A V T O B O R S A</h1>
+            <p className="landing-bottom">Avtoborsa is the place to find the car of your dreams. Find, buy and drive!</p>
+            <ul>
+                <li><i class="fa-solid fa-car-side"></i>Lots of cars</li>
+                <li><i class="fa-solid fa-clock"></i>Time Effecient</li>
+                <li><i class="fa-solid fa-universal-access"></i>Easily accessible</li>
+            </ul>
+            <a href="/">Find your car now <i class="fa-solid fa-magnifying-glass"></i></a>
         </section>
     )
 }
