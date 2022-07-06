fd404051d43b47a9cbbb576211c04a0a50035a23
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-07 01:02:03 +0300

    working on landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <LandingPage/>
       <Footer/>
     </div>
   );
--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './LandingPage.css';
+import landing from '../../assets/landing-1.jpg';
 
 function LandingPage(){
     return(
         <section className="landing">
-            
+            <img src={landing} alt=""/>
+            <h1 className="landing-up">Yet to find the car you want?</h1>
         </section>
     )
 }
+
+export default LandingPage;
