b1a0b374200a71030301b3fa2fb0743cbe0d60ec
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-11 21:34:01 +0300

    register design

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import LandingPage from './components/LandingPage/LandingPage';
 import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Login/>
+      <Register/>
       <Footer/>
     </div>
   );
---
+++ client/src/components/Register/Register.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Register.css';
+import background from '../../assets/auth-img.jpg'
+
+export const Register = () =>{
+    return(
+        <section className="register">
+            <img src={background} alt="" />
+            <section className="left">
+                <h1>Benefits of being a logged in user at Avtoborsa:</h1>
+                <ul>
+                    <li>You can like as many car listings as you want!</li>
+                    <li>You can list a car in our web app!</li>
+                    <li>And many more...</li>
+                    <li className="special">Come on, fill up the form and sign up!</li>
+                </ul>
+            </section>
+            <form action="">
+                <h1>Sign up</h1>
+                <div className="email">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" placeholder="Your email here"/>
+                </div>
+                <div className="password">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="Your password here"/>
+                </div>
+                <button type='submit'>Sign up!</button>
+            </form>
+        </section>
+    )
+}
