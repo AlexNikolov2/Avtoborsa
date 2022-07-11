494ab7eae769f0b331c3e070109846e42a75347d
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-11 19:38:48 +0300

    working on login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,3 +1,23 @@
 import React from 'react';
 import './Login.css';
-import {background} from '../../assets/auth-img.jpg'
+import background from '../../assets/auth-img.jpg'
+
+export const Login = () =>{
+    return(
+        <section className="login">
+            <img src={background} alt="" />
+            <form action="">
+                <h1>Log In</h1>
+                <div className="email">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" placeholder="Your email here"/>
+                </div>
+                <div className="password">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="Your password here"/>
+                </div>
+                <button type='submit'>Log in!</button>
+            </form>
+        </section>
+    )
+}
