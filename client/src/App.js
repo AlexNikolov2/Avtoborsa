30f687b671d3445ecc2bb9702225c8e0ee8b36bb
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-11 19:38:29 +0300

    working on login

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import LandingPage from './components/LandingPage/LandingPage';
+import { Login } from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <LandingPage/>
+      <Login/>
       <Footer/>
     </div>
   );
