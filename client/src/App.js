9852c143a89e117bba9d275446984fc19268ffcd
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-11 21:37:32 +0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import LandingPage from './components/LandingPage/LandingPage';
-import { Login } from './components/Login/Login';
+//import LandingPage from './components/LandingPage/LandingPage';
+//import { Login } from './components/Login/Login';
 import { Register } from './components/Register/Register';
 
 function App() {
