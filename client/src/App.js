ae0515efa60a15ad743b8291f9edc510a8763355
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-12 21:11:23 +0300

    modulization

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-//import LandingPage from './components/LandingPage/LandingPage';
+import Header from './components/common/Header/Header';
+import Footer from './components/common/Footer/Footer';
+//import LandingPage from './components/common/LandingPage/LandingPage';
 //import { Login } from './components/Login/Login';
-import { Register } from './components/Register/Register';
+import { Register } from './components/auth/Register/Register';
 
 function App() {
   return (
