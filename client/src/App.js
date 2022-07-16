34de57dfa81a310a966df3e209c6f423d9234388
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-16 23:57:44 +0300

    deinos

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import Header from './components/common/Header/Header';
 import Footer from './components/common/Footer/Footer';
+import { CreateListing } from './components/crud/CreateListing/CreateListing';
 import LandingPage from './components/common/LandingPage/LandingPage';
-import { Login } from './components/auth/Login/Login';
-import { Register } from './components/auth/Register/Register';
+//import { Login } from './components/auth/Login/Login';
+//import { Register } from './components/auth/Register/Register';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Login/>
+      <CreateListing/>
       <Footer/>
     </div>
   );
