32de6d68d1ec933a753ab5b95d836398ae1f87a2
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-17 22:40:17 +0300

    small fixes around components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Header from './components/common/Header/Header';
-import Footer from './components/common/Footer/Footer';
-import { CreateListing } from './components/crud/CreateListing/CreateListing';
-import LandingPage from './components/common/LandingPage/LandingPage';
+import {Header} from './components/common/Header/Header';
+import {Footer} from './components/common/Footer/Footer';
+//import { CreateListing } from './components/crud/CreateListing/CreateListing';
+//import {LandingPage} from './components/common/LandingPage/LandingPage';
 import {EditListing } from './components/crud/EditListing/EditListing'
 //import { Login } from './components/auth/Login/Login';
 //import { Register } from './components/auth/Register/Register';
