cf0852d96f0e69bd55c0f54964ebb8f31bd40eb4
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-17 23:05:20 +0300

    readme

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import {Header} from './components/common/Header/Header';
 import {Footer} from './components/common/Footer/Footer';
 //import { CreateListing } from './components/crud/CreateListing/CreateListing';
-//import {LandingPage} from './components/common/LandingPage/LandingPage';
-import {EditListing } from './components/crud/EditListing/EditListing'
+import {LandingPage} from './components/common/LandingPage/LandingPage';
+//import {EditListing } from './components/crud/EditListing/EditListing'
 //import { Login } from './components/auth/Login/Login';
 //import { Register } from './components/auth/Register/Register';
 
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      <EditListing/>
+      <LandingPage/>
       <Footer/>
     </div>
   );
--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 *The place to find your car!*
 
+![tito fc](./client/src/assets/Screenshot%202022-07-17%20230358.png)
+
 Nowadays it's not easy to find the car you want. Thousands of car brands, each with its own pros and cons. Not only this, but there are so many different autohouses around the country, which makes it even harder to find the car you want. That is the reason why Avtoborsa exists - to help you find the car you want.
 # About the site:
 
