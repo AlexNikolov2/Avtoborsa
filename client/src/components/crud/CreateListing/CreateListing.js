de61b15cdc23078a74bff52f5345d91a43bd1f7d
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-17 21:08:57 +0300

    create done

--- client/src/components/crud/CreateListing/CreateListing.js
+++ client/src/components/crud/CreateListing/CreateListing.js
@@ -1,14 +1,17 @@
 import './CreateListing.css'
+import image from '../../../assets/crud-img.jpg'
 
 export const CreateListing = () => {
   return (
     <section className="create">
-        <img src="" alt=""/>
+        <img src={image} alt=""/>
       <form>
         <input type="text" placeholder="Car name/model" />
-        <input type="number" placeholder="price" />
-        <input type="number" placeholder="year" />
-        <input type="file" />
+        <div className="numbers">
+          <input type="number" placeholder="price in $$$" />$
+          <input type="number" placeholder="year" />
+        </div>
+        <input type="file" id="file"/>
         <fieldset>
           <legend>Fuel type</legend>
           <div>
@@ -26,6 +29,7 @@ export const CreateListing = () => {
           </div>
         </fieldset>
         <textarea name="description" id="" cols="30" rows="10" placeholder="Description"></textarea>
+        <button>Create Listing</button>
       </form>
     </section>
   );
