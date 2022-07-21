121171c527045d747dd58b877b7ce9e993218229
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-21 14:10:22 +0300

    welp, I had to delete ze server

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-start();
-
-async function start() {
-    const app = express();
-
-    app.listen(3000, () => console.log('Server running on http://localhost:3030'));
-}
