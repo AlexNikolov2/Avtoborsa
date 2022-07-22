fed3944dbe0e10a3da5659768b1b63b87578e07e
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-22 15:31:09 +0300

    stuuff

--- client/src/components/catalog/particle/CatalogParticle.js
+++ client/src/components/catalog/particle/CatalogParticle.js
@@ -1,7 +1,14 @@
 import './CatalogParticle.css';
+import {image} from '../../../assets/car-test.jpg';
 
 export const CatalogParticle = () => {
     return(
-        <h3>Car</h3>
+        <section className="car">
+            <h3>Chevrolet</h3>
+            <img src={image} alt="" />
+            <p className="year">2006</p>
+            <p className="price">215000</p>
+            <p className="description"></p>
+        </section>
     )
 }
