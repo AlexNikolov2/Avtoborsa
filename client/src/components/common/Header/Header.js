1ae781d45ee8dab9b205c3e98b21f2aa507075de
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-23 17:47:34 +0300

    wow bro amazing

--- client/src/components/common/Header/Header.js
+++ client/src/components/common/Header/Header.js
@@ -10,14 +10,11 @@ export const Header = () => {
             </section>
             <ul>
                 <li><Link to="/home"><i class="fa-solid fa-house"></i>Home</Link></li>
-                <li><Link to="/login"><i class="fa-solid fa-right-to-bracket"></i>Login</Link></li>
-                <li><Link to="/register"><i class="fa-solid fa-user-plus"></i>Sign up</Link></li>
-                {//<li><Link to="/"><i class="fa-solid fa-square-plus"></i>Create</a></li>
-}
-                {//<li><Link to="/"><i class="fa-solid fa-address-card"></i>Profile</a></li>
-}
-                {//<li><Link to="/"><i class="fa-solid fa-right-from-bracket"></i>Log out</a></li>
-}
+                {/*<li><Link to="/login"><i class="fa-solid fa-right-to-bracket"></i>Login</Link></li>
+                <li><Link to="/register"><i class="fa-solid fa-user-plus"></i>Sign up</Link></li>*/}
+                <li><Link to="/create"><i class="fa-solid fa-square-plus"></i>Create</Link></li>
+                <li><Link to="/profile"><i class="fa-solid fa-address-card"></i>Profile</Link></li>
+                <li><Link to="/"><i class="fa-solid fa-right-from-bracket"></i>Log out</Link></li>
                 <li><Link to="/"><i class="fa-solid fa-magnifying-glass"></i>Search</Link></li>
             </ul>
         </header>
