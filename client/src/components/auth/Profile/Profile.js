f709551889aa995b56e88e3303fcb306bad3e665
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-23 17:06:10 +0300

    profile page done

--- client/src/components/auth/Profile/Profile.js
+++ client/src/components/auth/Profile/Profile.js
@@ -1,3 +1,23 @@
 import './Profile.css';
 import {CatalogParticle} from '../../catalog/particle/CatalogParticle'
 
+export const Profile = () => {
+    return (
+        <section className="profile">
+            <h1>Alex Nikolov's uploaded listings</h1>
+            <div className="cars">
+            <CatalogParticle/>
+            <CatalogParticle/>
+            <CatalogParticle/>
+            <CatalogParticle/>
+            <CatalogParticle/>
+            <CatalogParticle/>
+            <CatalogParticle/>
+            <CatalogParticle/>
+            <CatalogParticle/>
+            <CatalogParticle/>
+            </div>
+            
+        </section>
+    );
+}
