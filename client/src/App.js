c8c228d5803fd01cd222db2f2f4379732b0fad7b
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-23 17:07:50 +0300

    wow, funny moment

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {Header} from './components/common/Header/Header';
 import {Footer} from './components/common/Footer/Footer';
 //import { CreateListing } from './components/crud/CreateListing/CreateListing';
 //import {LandingPage} from './components/common/LandingPage/LandingPage';
-//import { Catalog } from './components/catalog/Catalog';
+import { Catalog } from './components/catalog/Catalog';
 import { Profile } from './components/auth/Profile/Profile';
 //import {EditListing } from './components/crud/EditListing/EditListing'
 //import { Login } from './components/auth/Login/Login';
