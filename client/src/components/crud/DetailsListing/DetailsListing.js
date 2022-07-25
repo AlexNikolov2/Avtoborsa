58da3ae09849b1a0c4e23559fb3316a9d4094ca0
Aleks Nikolov <tcr.official.world@gmail.com>
2022-07-26 00:00:16 +0300

    aaqa

---
+++ client/src/components/crud/DetailsListing/DetailsListing.js
@@ -0,0 +1,11 @@
+import './DetailsListing.css';
+import image from '../../../assets/car-test.jpg'
+
+export const DetailsListing = () => {
+    return (
+        <section className="details">
+            <h2>Chevrolet Chevron 1.6 HDI</h2>
+            <img src={image} alt="obichkam aleksandra" />
+        </section>
+    )
+};
