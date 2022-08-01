5e4e64d08d279be8982b0bc36f661e9faf3e6f3b
Aleks Nikolov <tcr.official.world@gmail.com>
2022-08-01 19:23:57 +0300

    continue the strike

--- client/src/components/common/Header/Header.js
+++ client/src/components/common/Header/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Header.css'
 import {Link} from 'react-router-dom';
+import { useContext } from 'react';
+import UserContext from '../../../contexts/UserContext'
 
 export const Header = () => {
+    const user = useContext(UserContext);
+
     return(
         <header className="">
             <section className="logo">
@@ -10,12 +14,22 @@ export const Header = () => {
             </section>
             <ul>
                 <li><Link to="/home"><i className="fa-solid fa-house"></i>Home</Link></li>
-                <li><Link to="/login"><i className="fa-solid fa-right-to-bracket"></i>Login</Link></li>
-                <li><Link to="/register"><i className="fa-solid fa-user-plus"></i>Sign up</Link></li>
-                <li><Link to="/create"><i className="fa-solid fa-square-plus"></i>Create</Link></li>
-                <li><Link to="/profile"><i className="fa-solid fa-address-card"></i>Profile</Link></li>
-                <li><Link to="/"><i className="fa-solid fa-right-from-bracket"></i>Log out</Link></li>
                 <li><Link to="/search"><i className="fa-solid fa-magnifying-glass"></i>Search</Link></li>
+                {
+                    user && 
+                    <>
+                        <li><Link to="/create"><i className="fa-solid fa-square-plus"></i>Create</Link></li>
+                        <li><Link to="/profile"><i className="fa-solid fa-address-card"></i>Profile</Link></li>
+                        <li><Link to="/"><i className="fa-solid fa-right-from-bracket"></i>Log out</Link></li>
+                    </>
+                }
+                {
+                    !user && 
+                    <>
+                        <li><Link to="/login"><i className="fa-solid fa-right-to-bracket"></i>Login</Link></li>
+                        <li><Link to="/register"><i className="fa-solid fa-user-plus"></i>Sign up</Link></li>
+                    </>
+                }
             </ul>
         </header>
     )
