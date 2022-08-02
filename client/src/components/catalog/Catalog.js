66bd2c12b9c703dfde7f9905e389f4918fecabf5
Aleks Nikolov <tcr.official.world@gmail.com>
2022-08-02 18:46:15 +0300

    PROKLET ADA BIDE SINKO TUGINATAAA

--- client/src/components/catalog/Catalog.js
+++ client/src/components/catalog/Catalog.js
@@ -2,6 +2,7 @@ import "./Catalog.css";
 import { CatalogParticle } from "./particle/CatalogParticle.js";
 import { useState, useEffect } from "react";
 import firebase from "../../config/firebase";
+import { Link } from "react-router-dom";
 
 export const Catalog = () => {
   const [products, setProducts] = useState([]);
@@ -14,13 +15,17 @@ export const Catalog = () => {
       .then((data) => {
         let arr = [];
         data.forEach((doc) => {
+          
           let info = doc.data();
           arr.push({
             id: doc.id,
-            info: info,
+            info: info
           });
+          
         });
+        
         setProducts(arr);
+        //console.log(arr);
       });
   }, []);
 
@@ -30,13 +35,25 @@ export const Catalog = () => {
       <div className="cars">
         {products.length > 0 ? (
           products.map((product) => {
-            return (
+            console.log(product.info);
+
+            return(
               <CatalogParticle
-                key={product.id}
-                id={product.id}
-                item={product.info}
-              />
-            );
+              key={product.id}
+              id={product.id}
+              item={product.info}
+            />
+            )
+            /*return(
+              <section className="car">
+                  <h3>{product.info.name}</h3>
+                  <img src={product.info.image} alt="" />
+                  <p className="year"><b>Year: </b>{product.info.year}</p>
+                  <p className="price"><b>Price: </b>{product.info.price} $</p>
+                  <Link to={`/details`}>Details</Link>
+              </section>
+              
+          )*/
           })
         ) : (
           <p>No listings yet</p>
--- client/src/components/catalog/particle/CatalogParticle.js
+++ client/src/components/catalog/particle/CatalogParticle.js
@@ -2,14 +2,16 @@ import './CatalogParticle.css';
 import image from '../../../assets/car-test.jpg';
 import { Link } from 'react-router-dom';
 
-export const CatalogParticle = () => {
+export const CatalogParticle = (item, id) => {
+    console.log(item);
     return(
         <section className="car">
-            <h3>Chevrolet Chevron 1.6 TDI</h3>
-            <img src={image} alt="" />
-            <p className="year"><b>Year: </b>2006</p>
-            <p className="price"><b>Price: </b>215000 $</p>
-            <Link to="/details">Details</Link>
+            <h3>{item.item.name}</h3>
+            <img src={item.item.image} alt="" />
+            <p className="year"><b>Year: </b>{item.item.year}</p>
+            <p className="price"><b>Price: </b>{item.item.price} $</p>
+            <Link to={`/details/${item.id}`}>Details</Link>
         </section>
+        
     )
 };
