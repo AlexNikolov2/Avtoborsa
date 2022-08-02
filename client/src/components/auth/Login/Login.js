0ba539639b12851adf71d93e414658824f1ca77d
Aleks Nikolov <tcr.official.world@gmail.com>
2022-08-02 16:00:12 +0300

    FUCKING AUTHENTICATION

--- client/src/components/auth/Login/Login.js
+++ client/src/components/auth/Login/Login.js
@@ -1,23 +1,54 @@
-import React from 'react';
-import './Login.css';
-import background from '../../../assets/auth-img.jpg'
+import React from "react";
+import "./Login.css";
+import background from "../../../assets/auth-img.jpg";
+import { useContext, useState } from "react";
+import UserContext from "../../../contexts/UserContext";
+import { Navigate } from "react-router-dom";
+import firebase from "../../../services/firebase";
 
-export const Login = () =>{
-    return(
-        <section className="login">
-            <img src={background} alt="" />
-            <form action="">
-                <h1>Log In</h1>
-                <div className="email">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" placeholder="Your email here"/>
-                </div>
-                <div className="password">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" placeholder="Your password here"/>
-                </div>
-                <button type='submit'>Log in!</button>
-            </form>
-        </section>
-    )
-}
+export const Login = () => {
+  const user = useContext(UserContext);
+  const [error, setError] = useState();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        if (err.code === "auth/user-not-found")
+          setError("There is no account corresponding to this email");
+        if (err.code === "auth/wrong-password") setError("Wrong password");
+        console.log(err);
+      });
+  };
+
+  if (user) {
+    return <Navigate to="/home" />;
+  } else {
+    return (
+      <section className="login">
+        <article>
+               <p className="err">{error}</p>
+            </article>
+        <img src={background} alt="" />
+        <form onSubmit={onSubmit}>
+          <h1>Log In</h1>
+          <div className="email">
+            <label htmlFor="email">Email</label>
+            <input type="text" placeholder="Your email here" />
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="Your password here" />
+          </div>
+          <button type="submit">Log in!</button>
+        </form>
+      </section>
+    );
+  }
+};
--- client/src/components/auth/Register/Register.js
+++ client/src/components/auth/Register/Register.js
@@ -1,95 +1,111 @@
-import React from 'react';
-import './Register.css';
-import background from '../../../assets/auth-img.jpg'
-import { useContext, useState } from 'react';
+import React from "react";
+import "./Register.css";
+import background from "../../../assets/auth-img.jpg";
+import { useContext, useState } from "react";
 
-import UserContext from '../../../contexts/UserContext';
-import { Navigate } from 'react-router-dom';
-import firebase from '../../../services/firebase'
+import UserContext from "../../../contexts/UserContext";
+import { Navigate } from "react-router-dom";
+import firebase from "../../../services/firebase";
 
-export const Register = () =>{
-    const user = useContext(UserContext);
-    const [errors, setErrors] = useState([]);
+export const Register = () => {
+  const user = useContext(UserContext);
+  const [errors, setErrors] = useState([]);
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-  
-        let email = e.target.email.value;
-        let password = e.target.password.value;
-        let errs = [];
-        let isValidEmail = /^[a-zA-Z0-9.-]{4,}@\w+\.com$/.test(email);
-  
-        if (password && password.length < 6) errs.push("Your password should be at least 6 characters long!");
-        if (email && !isValidEmail) errs.push("Invalid email!");
-        if (!password) errs.push("You forgot to fill up password!")
-        if (!email) errs.push("You forgot to fill up email!")
-  
-        setErrors(errs);
-  
-        if (errs.length > 0) {
-           return;
-        }
-  
-        
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    let email = e.target.email.value;
+    let password = e.target.password.value;
+    let errs = [];
+    let isValidEmail = /^[a-zA-Z0-9.-]{4,}@\w+\.com$/.test(email);
+
+    if (password && password.length < 6)
+      errs.push("Your password should be at least 6 characters long!");
+    if (email && !isValidEmail) errs.push("Invalid email!");
+    if (!password) errs.push("You forgot to fill up password!");
+    if (!email) errs.push("You forgot to fill up email!");
+
+    setErrors(errs);
+
+    if (errs.length > 0) {
+      return;
+    }
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((credentials) => {
+        let currUser = firebase.auth().currentUser;
+        const user = credentials.user;
+        const id = currUser.uid;
+        const listings = [];
         firebase
-           .auth()
-           .createUserWithEmailAndPassword(email, password)
-           .then(() => {
-              let currUser = firebase.auth().currentUser;
-              firebase.firestore.collection('Users').doc(currUser.id).set();
-             
-  
-              currUser
-                 .then(() => {
-                    
-                    firebase.firestore().collection("Listings").doc(email).set({ listings: [] });
-                 });
-           })
-           .catch((err) => {
-              setErrors([err.message]);
-           });
-     };
+          .firestore()
+          .collection("Users")
+          .doc(currUser.uid)
+          .set({ id, email, listings })
+          .then(this.checkStatus({ user }));
+      })
+      .catch((err) => {
+        setErrors([err.message]);
+      });
+  };
 
-    if(user){
-        return <Navigate to="/home"/>;
-    }
-    else{
-        return(
-            <section className="register">
-                {errors.length < 1 ? (
-               ""
-            ) : (
-               <article>
-                  {errors?.map((err) => {
-                     return <p className="err" key={err}>{err}</p>;
-                  })}
-               </article>
-            )}
-                <img src={background} alt="" />
-                <section className="left">
-                    <h1>Benefits of being a logged in user at Avtoborsa:</h1>
-                    <ul>
-                        <li><i className="fa-solid fa-car-side"></i>You can like as many car listings as you want!</li>
-                        <li><i className="fa-solid fa-car-side"></i>You can list a car in our web app!</li>
-                        <li><i className="fa-solid fa-car-side"></i>And many more...</li>
-                        <li className="special">Come on, fill up the form and sign up!</li>
-                    </ul>
-                </section>
-                <form onSubmit={onSubmit}>
-                    <h1>Sign up</h1>
-                    <div className="email">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" placeholder="Your email here"/>
-                    </div>
-                    <div className="password">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="Your password here"/>
-                    </div>
-                    <button className="button" type='submit'>Sign up!</button>
-                </form>
-            </section>
-        )
-    }
-
-    
-}
+  if (user) {
+    return <Navigate to="/home" />;
+  } else {
+    return (
+      <section className="register">
+        {errors.length < 1 ? (
+          ""
+        ) : (
+          <article>
+            {errors?.map((err) => {
+              return (
+                <p className="err" key={err}>
+                  {err}
+                </p>
+              );
+            })}
+          </article>
+        )}
+        <img src={background} alt="" />
+        <section className="left">
+          <h1>Benefits of being a logged in user at Avtoborsa:</h1>
+          <ul>
+            <li>
+              <i className="fa-solid fa-car-side"></i>You can like as many car
+              listings as you want!
+            </li>
+            <li>
+              <i className="fa-solid fa-car-side"></i>You can list a car in our
+              web app!
+            </li>
+            <li>
+              <i className="fa-solid fa-car-side"></i>And many more...
+            </li>
+            <li className="special">Come on, fill up the form and sign up!</li>
+          </ul>
+        </section>
+        <form onSubmit={onSubmit}>
+          <h1>Sign up</h1>
+          <div className="email">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" placeholder="Your email here" />
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Your password here"
+            />
+          </div>
+          <button className="button" type="submit">
+            Sign up!
+          </button>
+        </form>
+      </section>
+    );
+  }
+};
