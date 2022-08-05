107cef8d6147e3311649db441fecce1ee6fd30e7
Aleks Nikolov <tcr.official.world@gmail.com>
2022-08-05 18:28:12 +0300

    SEARCH IS READY!

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -29,11 +29,37 @@ export const Search = () => {
       });
   }, []);
 
+  const search = (val) => {
+    firebase
+       .firestore()
+       .collection("Listings")
+       .get()
+       .then((data) => {
+          let arr = [];
+          data.forEach((doc) => {
+             let info = doc.data();
+             arr.push({
+                id: doc.id,
+                info: info,
+             });
+          });
+          let filtered = arr.filter((x) => {
+             return x.info.product.name.toLowerCase().includes(val.toLowerCase());
+          });
+          setProducts(filtered);
+       });
+ };
+
+ const onSubmit = e => {     
+  e.preventDefault()
+    search(e.target.name.value);
+  };
+
   return (
     <section className="search">
       <h1>Search Page</h1>
-      <form action="">
-        <input type="text" placeholder="Car name/model" className="model"/>
+      <form onSubmit={onSubmit}>
+        <input name="name" type="text" placeholder="Car name/model" className="model"/>
         <button>Search <i className="fa-solid fa-magnifying-glass"></i></button>
       </form>
       <article className="results">
@@ -45,7 +71,7 @@ export const Search = () => {
               <CatalogParticle
               key={product.id}
               id={product.id}
-              item={product.info}
+              item={product.info.product}
             />
             )
           })
