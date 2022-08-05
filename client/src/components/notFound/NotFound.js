fa0026d7a4f96bca6246834eaa743f57c92ade55
Aleks Nikolov <tcr.official.world@gmail.com>
2022-08-05 19:02:55 +0300

    stylization of 404

--- client/src/components/notFound/NotFound.js
+++ client/src/components/notFound/NotFound.js
@@ -1,7 +1,15 @@
 import './NotFound.css'
+import image from '../../assets/not-found.jpg'
+import {Link} from 'react-router-dom';
 
 export const NotFound = () => {
     return(
-    <p>404 Not Found</p>
+    <section className="notFound">
+        <img src={image} alt="" />
+        <p>404 Not Found</p>
+        <p className="reason">The route you have selected is invalid.</p>
+        <Link to="/home">Now let's go back to home <i className="fa-solid fa-house"></i></Link>
+    </section>
+    
     )
 }
