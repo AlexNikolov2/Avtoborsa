9e1cf6af9450ae7f375f53851d93150df697d86e
Aleks Nikolov <tcr.official.world@gmail.com>
2022-09-10 11:48:25 +0300

    some nice fix on my project

--- client/src/components/catalog/particle/CatalogParticle.js
+++ client/src/components/catalog/particle/CatalogParticle.js
@@ -1,17 +1,29 @@
 import './CatalogParticle.css';
 import { Link } from 'react-router-dom';
 import UserContext from '../../../contexts/UserContext'
-import {useContext} from 'react';
+import {useContext, useState} from 'react';
 
 export const CatalogParticle = ({id, item}) => {
     const user = useContext(UserContext);
+    const [isShown, setIsShown] = useState(false);
+
     return(
-        <section className="car">
+        <section className="car"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}>
+            
+                
             <h3>{item.name}</h3>
             <img src={item.imageUrl} alt="" />
             <p className="year"><b>Year: </b>{item.year}</p>
             <p className="price"><b>Price: </b>{item.price} $</p>
-            {user?<Link to={`/details/${id}`}>Details</Link> : ""}
+            {isShown && (
+            <div>
+            {user?<Link  to={`/details/${id}`}>
+            
+          Details</Link> : ""}
+          </div>
+          )}
         </section>
         
     )
